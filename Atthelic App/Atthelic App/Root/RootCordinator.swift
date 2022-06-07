827a0ff906660c99ac95fcd6565092509440ce20
Manu Martinez <attlos.global@Emanuels-Air.home>
2022-06-07 16:02:47 +0200

    Design base structure and create main entry point

--- Atthelic App/Atthelic App/Root/RootCordinator.swift
+++ Atthelic App/Atthelic App/Root/RootCordinator.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import UIKit
+
+class RootCoordinator {
+    
+    // MARK: - Dependencies
+    let window: UIWindow
+    let navigationController: UINavigationController
+    let factory: RootFactory
+    
+    // MARK: - Inits
+    init(_ window: UIWindow, navigationController: UINavigationController, factory: RootFactory) {
+        self.window = window
+        self.navigationController = navigationController
+        self.factory = factory
+    }
+    
+    // MARK: - Methods
+    func start() {
+        let scene = factory.createMainModule(self)
+        navigationController.viewControllers = [scene]
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+    }
+    
+}
+
+// MARK: - Implementing Navigation Stuff
+extension RootCoordinator: MainViewRouter {
+    
+}
---
+++ Atthelic App/Atthelic App/Shared/Extensions/String+Extension.swift
@@ -0,0 +1,10 @@
+//
+//  String+Extension.swift
+//  Atthelic App
+//
+//  Created by Emanuel Martinez on 7/6/22.
+//
+
+import Foundation
+
+extension String: Error {}
